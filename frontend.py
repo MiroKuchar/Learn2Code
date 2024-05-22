c8454350708606bc7a0a53fc13cbe676ff3f5d5f
Miroslav Kuchar <miroslav.kuchar@outlook.com>
2024-05-22 21:49:55 +0200

    Changed Validation Message

--- frontend.py
+++ frontend.py
@@ -18,7 +18,7 @@ def main():
             print("Thank you for playing!")
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("Your Selection was Invalid. Please try again.")
 
 if __name__ == "__main__":
     main()
